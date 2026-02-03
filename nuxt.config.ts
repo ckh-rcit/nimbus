452ffa31c3ab257ac4abb33916a06d6931f5970a
Chris Harris <chharris@rivco.org>
2026-02-03 15:31:47 -0800

    Add VueUse for refDebounced utility

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/icon'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/icon', '@vueuse/nuxt'],
 
   // Import Tailwind CSS
   css: ['~/assets/css/main.css'],
